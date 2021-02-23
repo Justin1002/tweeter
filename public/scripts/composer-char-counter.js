7971a5288c97c06873635888b3f2b8cd6530e354
Justin Ly <jly3@ualberta.ca>
2021-02-23 18:09:29 +0000

    fix nav bar font sizing

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $('#tweet-text').on('input',function() {
     $(this).parent().find('.counter').text(140-$(this).val().length)
 
-    let length = Number($(this).parent().find('.counter').text())
-    if (length < 0) {
+    let characters = $(this).parent().find('.counter').text()
+    if (characters < 0) {
       $('.counter').css("color","red")
     }
     else {
